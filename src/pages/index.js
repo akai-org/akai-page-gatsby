7f0eaf1db78b178d826a55894f770cdc90e645b1
Ryszard Poznański <ryszard.poznanski0@gmail.com>
2020-02-21 01:14:33 +0100

    Include layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 const IndexPage = () => (
-  <>
+  <Layout>
     <h1>Index Page</h1>
-  </>
+  </Layout>
 )
 
 export default IndexPage
